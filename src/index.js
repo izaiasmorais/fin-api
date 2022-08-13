945087bad1e55b2d78c4802aaf8715a0ac8b21ac
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-08-13 19:21:45 -0300

    feat: Setup[

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { v4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+// Middleware
+function verifyCpfAccountExists(req, res, next) {
+  
+}
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const custumerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (custumerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: v4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  return response.json(customer.statement);
+});
+
+app.listen(3333);
