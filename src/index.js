5fe01dfefe7bae0fd25ee1e3f9da70f02059c750
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-08-18 22:50:01 -0300

    chore: customer creation, middlewares and deposits following business rules

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ app.use(express.json());
 const customers = [];
 
 // Middleware
-function verifyCpfAccountExists(req, res, next) {
-  
+function verifyCpfAccountExists(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
 }
 
 app.post("/account", (request, response) => {
@@ -33,16 +43,28 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
+// app.use(verifyCpfAccountExists);
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" });
-  }
-
+app.get("/statement", verifyCpfAccountExists, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyCpfAccountExists, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
