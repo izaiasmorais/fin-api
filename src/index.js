7f9eca08d59d333a16ce2ad8663886e9db061f93
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-08-19 20:59:58 -0300

    chore: Get statements by date

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 const customers = [];
 
 // Middleware
+
 function verifyCpfAccountExists(request, response, next) {
   const { cpf } = request.headers;
 
@@ -65,6 +66,21 @@ app.post("/deposit", verifyCpfAccountExists, (request, response) => {
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
+});
+
+app.get("/statement/date", verifyCpfAccountExists, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  // 10/10/2022
+
+  const statement = customer.statement.filter((statement) => {
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString();
+  });
+
+  return response.json(statement);
 });
 
 app.listen(3333);
